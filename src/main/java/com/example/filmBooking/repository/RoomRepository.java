2ad9e712e1911ca3f3af7de4020793660397a935
HoangToan2903 <113886266+HoangToan2903@users.noreply.github.com>
2023-08-17 12:40:16 +0700

    update prọect (#10)
    Co-authored-by: Hoangtoan2908 <107165535+Hoangtoan2908@users.noreply.github.com>

---
+++ src/main/java/com/example/filmBooking/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.example.filmBooking.repository;
+
+import com.example.filmBooking.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RoomRepository  extends JpaRepository<Room, UUID> {
+}
