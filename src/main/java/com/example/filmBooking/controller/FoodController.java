8272e64f08acd2334940ce82e8087c47e4c26ae4
TranThiDieuLinh <110156879+TranThiDieuLinh@users.noreply.github.com>
2023-09-03 10:31:49 +0700

    Linh update controller

--- src/main/java/com/example/filmBooking/controller/FoodController.java
+++ src/main/java/com/example/filmBooking/controller/FoodController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import java.util.UUID;
 
 @Controller
 @RequestMapping("/filmbookings")
+@CrossOrigin("*")
 @Tag(name = "food")
 public class FoodController {
     @Autowired
