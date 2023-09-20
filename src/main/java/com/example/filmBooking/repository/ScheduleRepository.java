46af2d2155c4a1fa41de7ae7695c5d221d92b5a9
TranDieuLinh2003 <137884249+TranDieuLinh2003@users.noreply.github.com>
2023-09-20 09:36:22 +0700

    Update ScheduleRepository.java

--- src/main/java/com/example/filmBooking/repository/ScheduleRepository.java
+++ src/main/java/com/example/filmBooking/repository/ScheduleRepository.java
@@ -3,8 +3,6 @@ package com.example.filmBooking.repository;
 import com.example.filmBooking.model.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface ScheduleRepository extends JpaRepository<Schedule, UUID> {
+public interface ScheduleRepository extends JpaRepository<Schedule, String> {
 
 }
