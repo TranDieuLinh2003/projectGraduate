75dbd3616925b3f3ef143678d4df336d48bfac95
HoangToan2903 <113886266+HoangToan2903@users.noreply.github.com>
2023-10-06 11:55:21 +0700

    Update ScheduleApi.java

--- src/main/java/com/example/filmBooking/apis/ScheduleApi.java
+++ src/main/java/com/example/filmBooking/apis/ScheduleApi.java
@@ -1,42 +1,58 @@
 package com.example.filmBooking.apis;
 
+import com.example.filmBooking.model.Cinema;
+import com.example.filmBooking.model.Movie;
+import com.example.filmBooking.model.dto.DtoMovie;
 import com.example.filmBooking.model.dto.ScheduleDto;
+import com.example.filmBooking.repository.CinemaRepository;
+import com.example.filmBooking.repository.MovieRepository;
+import com.example.filmBooking.repository.ScheduleRepository;
 import com.example.filmBooking.service.ScheduleService;
+import com.example.filmBooking.service.impl.CinemaServiceImpl;
+import com.example.filmBooking.service.impl.MovieServiceImpl;
+import com.example.filmBooking.service.impl.ScheduleServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/api/schedule")
 public class ScheduleApi {
+
     @Autowired
-    private ScheduleService scheduleService;
+    private ScheduleServiceImpl scheduleService;
 
-    @GetMapping("/time-at")
-    public List<String> getTime(@RequestParam String movieId, @RequestParam String cinemaId ) {
-        return scheduleService.getTimeAt(movieId,cinemaId);
-    }
-    @GetMapping("/startAt-at")
-    public List<String> getstartAt(@RequestParam String movieId, @RequestParam String cinemaId) {
-        return scheduleService.getStartAt(movieId,cinemaId);
-    }
-    @GetMapping("/finishAt-at")
-    public List<String> getgetFinishAt(@RequestParam String movieId, @RequestParam String cinemaId) {
-        return scheduleService.getStartAt(movieId,cinemaId);
+    @Autowired
+    private CinemaServiceImpl cinemaService;
+
+    @Autowired
+    private MovieServiceImpl movieService;
+
+    @GetMapping("/start_at")
+    public List<String> getStart_At(@RequestParam String movieId, @RequestParam String cinemaId ) {
+        return scheduleService.getStart_At(movieId,cinemaId);
     }
 
-//    @GetMapping
-//    public List<ScheduleDto> getSchedules(@RequestParam String movieId, @RequestParam String cinemaId,
-//                                          @RequestParam String startAt,
-//                                          @RequestParam String roomId){
-//        return scheduleService.getSchedules(movieId,cinemaId,startAt,roomId);
-//    }
 
+    @GetMapping("/cinema_name")
+    private ResponseEntity<List<Cinema>> getCinema(@RequestParam String movieId, @RequestParam String cinemaId){
+        return new ResponseEntity<>(cinemaService.getCinemaAndMovie(movieId, cinemaId), HttpStatus.OK);
+    }
 
+    @GetMapping("/movie_name")
+    private ResponseEntity<List<Movie>> getMovie(@RequestParam String movieId, @RequestParam String cinemaId){
+        return new ResponseEntity<>(movieService.getMovie(movieId, cinemaId), HttpStatus.OK);
+    }
+
+    @GetMapping("/time")
+    private List<String> getTime(@RequestParam String movieId, @RequestParam String cinemaId, @RequestParam String start_at){
+        return scheduleService.getStart_At_Time(movieId, cinemaId,start_at);
+    }
 }
