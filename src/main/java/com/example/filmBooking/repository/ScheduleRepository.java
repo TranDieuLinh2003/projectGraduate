d9666710567721866010fce83fe6fdccf5598378
HoangToan2903 <113886266+HoangToan2903@users.noreply.github.com>
2023-10-06 11:42:41 +0700

    Update ScheduleRepository.java
    toan

--- src/main/java/com/example/filmBooking/repository/ScheduleRepository.java
+++ src/main/java/com/example/filmBooking/repository/ScheduleRepository.java
@@ -12,25 +12,25 @@ import java.util.List;
 
 
 public interface ScheduleRepository extends JpaRepository<Schedule, String> {
-//            @Query("SELECT s.startAt, s.finishAt \n" +
-//                "FROM Cinema c\n" +
-//                "JOIN Room r ON c.id = r.cinema.id\n" +
-//                "JOIN Schedule s ON r.id = s.room.id\n" +
-//                "JOIN Movie m ON s.movie.id = m.id\n" +
-//                "where m.id= ?1 and c.id=?2")
-    @Query("SELECT DISTINCT s.startAt FROM Schedule s WHERE s.movie.id=:movieId AND s.room.cinema.id" +
-            "= :cinemaId ")
+ String Start_at = ("SELECT DISTINCT CONCAT(dayofweek(s.start_at), ' - ', DATE_FORMAT(s.start_at,  '%d/%m'))\n" +
+            "FROM projectLinh.cinema c\n" +
+            "JOIN projectLinh.room r ON c.id = r.cinema_id\n" +
+            "JOIN projectLinh.schedule s ON r.id = s.room_id\n" +
+            "JOIN projectLinh.movie m ON s.movie_id = m.id\n" +
+            "where c.id=:cinemaId and m.id=:movieId");
+    @Query(value = Start_at, nativeQuery = true)
     List<String> getstartAtAndFinishAt(@Param("movieId") String movieId
             , @Param("cinemaId") String cinemaId);
 
+//   lấy ra thời gian
     String time = ("\n" +
-            "\tselect distinct   DATE_FORMAT(s.start_at , '%H:%i')\n" +
-            "\tfrom projectLinh.cinema c\n" +
-            "\tjoin projectLinh.room r on r.cinema_id = c.id\n" +
-            "\tjoin projectLinh.schedule s on s.room_id = r.id\n" +
-            "\tjoin projectLinh.movie m on m.id = s.movie_id\n" +
-            "where m.id=:movieId\n" +
-            " and c.id =:cinemaId and CONCAT(dayofweek(s.start_at), ' - ', DATE_FORMAT(s.start_at,  '%d/%m'))=:start_at");
+        "\tselect distinct   DATE_FORMAT(s.start_at , '%H:%i')\n" +
+        "\tfrom projectLinh.cinema c\n" +
+        "\tjoin projectLinh.room r on r.cinema_id = c.id\n" +
+        "\tjoin projectLinh.schedule s on s.room_id = r.id\n" +
+        "\tjoin projectLinh.movie m on m.id = s.movie_id\n" +
+        "where m.id=:movieId\n" +
+        " and c.id =:cinemaId and CONCAT(dayofweek(s.start_at), ' - ', DATE_FORMAT(s.start_at,  '%d/%m'))=:start_at");
     @Query(value = time, nativeQuery = true)
     List<String> getTime(@Param("movieId") String movieId,
                          @Param("cinemaId") String cinemaId,
