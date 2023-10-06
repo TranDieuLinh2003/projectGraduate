59039f5c5f4d54a17e94601cdd61d30cecc757ca
HoangToan2903 <113886266+HoangToan2903@users.noreply.github.com>
2023-10-06 11:55:02 +0700

    Update CinemaApi.java
    toan

--- src/main/java/com/example/filmBooking/apis/CinemaApi.java
+++ src/main/java/com/example/filmBooking/apis/CinemaApi.java
@@ -2,6 +2,7 @@ package com.example.filmBooking.apis;
 
 import com.example.filmBooking.model.Cinema;
 import com.example.filmBooking.repository.CinemaRepository;
+import com.example.filmBooking.service.impl.CinemaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,14 +15,19 @@ import java.util.List;
 @RequestMapping(value="/api/cinema")
 public class CinemaApi {
     @Autowired
-    private CinemaRepository cinemaRepository;
+    private CinemaServiceImpl service;
 
 
     @GetMapping
     private ResponseEntity<List<Cinema>> getCinemaThatShowTheMovie(@RequestParam String movieId){
-        return new ResponseEntity<>(cinemaRepository.getCinemaThatShowTheMovie(movieId), HttpStatus.OK);
+        return new ResponseEntity<>(service.getCinema(movieId), HttpStatus.OK);
     }
 
+
+//    @GetMapping("/cinema_name")
+//    private ResponseEntity<List<Cinema>> getCinema(@RequestParam String movieId, @RequestParam String cinemaId){
+//        return new ResponseEntity<>(cinemaRepository.getCinema(movieId, cinemaId), HttpStatus.OK);
+//    }
 
 
 }
