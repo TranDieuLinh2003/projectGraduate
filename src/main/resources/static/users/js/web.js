ded577792ced1be91c97d7908e40da5c21a61f34
HoangToan2903 <113886266+HoangToan2903@users.noreply.github.com>
2023-10-06 11:57:25 +0700

    Update web.js
    toan

--- src/main/resources/static/users/js/web.js
+++ src/main/resources/static/users/js/web.js
@@ -73,33 +73,33 @@
 // // lịch chiếu
 //
 
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
-
-const tabActive = $('.tab-item.active');
-const line = $('.tabs .line')
-
-line.style.left = tabActive.offsetLeft  +'px';
-line.style.width = tabActive.offsetWidth  +'px';
-
-
-const tabs = $$('.tab-item')
-const panes = $$('.tab-pane')
-
-tabs.forEach((tab,index) => {
-    const pane = panes[index];
-
-    tab.onclick = function (){
-        $('.tab-item.active').classList.remove('active');
-        $('.tab-pane.active').classList.remove('active');
-
-        line.style.left = this.offsetLeft - 5 +'px';
-        line.style.width = this.offsetWidth  +'px';
-
-        this.classList.add('active');
-        pane.classList.add('active');
-    }
-});
+// const $ = document.querySelector.bind(document)
+// const $$ = document.querySelectorAll.bind(document)
+//
+// const tabActive = $('.tab-item.active');
+// const line = $('.tabs .line')
+//
+// line.style.left = tabActive.offsetLeft  +'px';
+// line.style.width = tabActive.offsetWidth  +'px';
+//
+//
+// const tabs = $$('.tab-item')
+// const panes = $$('.tab-pane')
+//
+// tabs.forEach((tab,index) => {
+//     const pane = panes[index];
+//
+//     tab.onclick = function (){
+//         $('.tab-item.active').classList.remove('active');
+//         $('.tab-pane.active').classList.remove('active');
+//
+//         line.style.left = this.offsetLeft - 5 +'px';
+//         line.style.width = this.offsetWidth  +'px';
+//
+//         this.classList.add('active');
+//         pane.classList.add('active');
+//     }
+// });
 
 
 //
