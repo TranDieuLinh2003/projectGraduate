39e72f80babe1db0d06fb604019b16744f3ca4a6
TranDieuLinh2003 <137884249+TranDieuLinh2003@users.noreply.github.com>
2023-10-10 12:59:36 +0700

    Update Schedule.java
    linh

--- src/main/java/com/example/filmBooking/model/Schedule.java
+++ src/main/java/com/example/filmBooking/model/Schedule.java
@@ -16,6 +16,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
