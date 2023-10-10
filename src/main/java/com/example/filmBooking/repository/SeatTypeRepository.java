2a46e9a2e0a8d3799920264f24254c1cc32f21eb
TranThiDieuLinh <110156879+TranThiDieuLinh@users.noreply.github.com>
2023-10-10 17:17:38 +0700

    Linh update model

--- src/main/java/com/example/filmBooking/repository/SeatTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.filmBooking.repository;
-
-import com.example.filmBooking.model.SeatType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface SeatTypeRepository extends JpaRepository<SeatType, String> {
-}
