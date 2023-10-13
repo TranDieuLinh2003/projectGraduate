e6afbcdd73bf2805dcd2a5c67dbd97047178eedd
DESKTOP-JC8STTK\admin <toanhd290803@gmail.com>
2023-10-13 11:14:11 +0700

    upload file

--- src/main/java/com/example/filmBooking/repository/ScheduleRepository.java
+++ src/main/java/com/example/filmBooking/repository/ScheduleRepository.java
@@ -5,36 +5,54 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 
 
 public interface ScheduleRepository extends JpaRepository<Schedule, String> {
- String Start_at = ("SELECT DISTINCT CONCAT(dayofweek(s.start_at), ' - ', DATE_FORMAT(s.start_at,  '%d/%m'))\n" +
+    String Start_at = ("SELECT DISTINCT  DATE(s.start_at)\n" +
             "FROM projectLinh.cinema c\n" +
             "JOIN projectLinh.room r ON c.id = r.cinema_id\n" +
             "JOIN projectLinh.schedule s ON r.id = s.room_id\n" +
             "JOIN projectLinh.movie m ON s.movie_id = m.id\n" +
             "where c.id=:cinemaId and m.id=:movieId");
+
     @Query(value = Start_at, nativeQuery = true)
     List<String> getstartAtAndFinishAt(@Param("movieId") String movieId
             , @Param("cinemaId") String cinemaId);
 
-//   lấy ra thời gian
+    //   lấy ra thời gian
     String time = ("\n" +
-        "\tselect distinct   DATE_FORMAT(s.start_at , '%H:%i')\n" +
-        "\tfrom projectLinh.cinema c\n" +
-        "\tjoin projectLinh.room r on r.cinema_id = c.id\n" +
-        "\tjoin projectLinh.schedule s on s.room_id = r.id\n" +
-        "\tjoin projectLinh.movie m on m.id = s.movie_id\n" +
-        "where m.id=:movieId\n" +
-        " and c.id =:cinemaId and CONCAT(dayofweek(s.start_at), ' - ', DATE_FORMAT(s.start_at,  '%d/%m'))=:start_at");
+            "\tselect distinct   DATE_FORMAT(s.start_at , '%H:%i')\n" +
+            "\tfrom projectLinh.cinema c\n" +
+            "\tjoin projectLinh.room r on r.cinema_id = c.id\n" +
+            "\tjoin projectLinh.schedule s on s.room_id = r.id\n" +
+            "\tjoin projectLinh.movie m on m.id = s.movie_id\n" +
+            "where m.id=:movieId\n" +
+            " and c.id =:cinemaId and  DATE(s.start_at)=:start_at");
+
     @Query(value = time, nativeQuery = true)
     List<String> getTime(@Param("movieId") String movieId,
                          @Param("cinemaId") String cinemaId,
                          @Param("start_at") String start_at);
+
+    //   lấy ra thời gian
+    String schedule = ("SELECT DISTINCT s.id, s.code, s.name, s.price, s.start_at, s.finish_at, s.status, s.room_id, s.movie_id\n" +
+            "            FROM projectLinh.cinema c\n" +
+            "            join projectLinh.room r ON c.id = r.cinema_id\n" +
+            "            join projectLinh.schedule s ON r.id = s.room_id\n" +
+            "            join projectLinh.movie m ON s.movie_id = m.id\n" +
+            "            join projectLinh.ticket t on t.schedule_id = s.id\n" +
+            "            join projectLinh.seat se on se.id= t.seat_id\n" +
+            "            WHERE c.id = :cinemaId AND m.id = :movieId\n" +
+            "            AND DATE(s.start_at ) = :startAt \n" +
+            "            AND DATE_FORMAT(s.start_at, '%H:%i') = :startTime");
+    @Query(value = schedule, nativeQuery = true)
+    List<Schedule> getSchedule(@Param("movieId") String movieId,
+                               @Param("cinemaId") String cinemaId,
+                               @Param("startAt") String startAt,
+                               @Param("startTime") String startTime);
+
+
 
     @Query(value = "select*from schedule ORDER BY start_at ASC", nativeQuery = true)
     List<Schedule> findAll();
