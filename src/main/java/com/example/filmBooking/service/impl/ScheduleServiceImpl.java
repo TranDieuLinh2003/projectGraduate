c623d322be4b2e06c1924488edf639589d2780e6
TranDieuLinh2003 <137884249+TranDieuLinh2003@users.noreply.github.com>
2023-10-18 10:19:20 +0700

    Update ScheduleServiceImpl.java

--- src/main/java/com/example/filmBooking/service/impl/ScheduleServiceImpl.java
+++ src/main/java/com/example/filmBooking/service/impl/ScheduleServiceImpl.java
@@ -1,19 +1,24 @@
 package com.example.filmBooking.service.impl;
 
+import com.example.filmBooking.model.GeneralSetting;
 import com.example.filmBooking.model.Movie;
 import com.example.filmBooking.model.Room;
 import com.example.filmBooking.model.Schedule;
+import com.example.filmBooking.repository.GeneralSettingRepository;
 import com.example.filmBooking.repository.MovieRepository;
 import com.example.filmBooking.repository.RoomRepository;
 import com.example.filmBooking.repository.ScheduleRepository;
 import com.example.filmBooking.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -30,6 +35,9 @@ public class ScheduleServiceImpl implements ScheduleService {
     @Autowired
     private RoomRepository roomRepository;
 
+    @Autowired
+    private GeneralSettingRepository settingRepository;
+
 
     @Override
     public List<Schedule> findByNameContains(String name) {
@@ -38,7 +46,15 @@ public class ScheduleServiceImpl implements ScheduleService {
 
     @Override
     public List<Schedule> findAll() {
+        generateSchedule();
         return repository.findAll();
+    }
+
+    // lấy thông tin cài đặt
+    public GeneralSetting findByIdSetting() {
+        String id = "613d6a30-167e-4b7c-985e-b510dc9bae25";
+        GeneralSetting setting = settingRepository.findById(id).get();
+        return setting;
     }
 
     @Override
@@ -59,25 +75,30 @@ public class ScheduleServiceImpl implements ScheduleService {
         //lấy thời lượng phim( đơn vị phút)
         Integer movieDuration = movie.getMovieDuration();
         Timestamp timestamp = Timestamp.valueOf(startAt);
-        timestamp.setTime(timestamp.getTime() + movieDuration * 60000 + 900000);
+        timestamp.setTime(timestamp.getTime() + movieDuration * 60000 + findByIdSetting().getBreakTime());
         LocalDateTime finishAt = timestamp.toLocalDateTime();
         schedule.setFinishAt(finishAt);
-        if (checkScheduleConflict(schedule, schedule.getRoom().getId()) && timeSchedule(schedule) && dateSchedule(schedule.getMovie().getId(), schedule)) {
+        schedule.setPrice(checkTheDayOfTheWeek(schedule));
+        String id = null;
+        if (checkScheduleConflict(schedule, schedule.getRoom().getId())
+                && timeSchedule(schedule, findByIdSetting().getBusinessHours(), findByIdSetting().getCloseTime())
+                && dateSchedule(schedule.getMovie().getId(), schedule)) {
             // Lưu suất chiếu mới vào cơ sở dữ liệu
-            repository.save(schedule);
+            id = repository.save(schedule).getId();
             System.out.println("Lưu suất chiếu mới thành công.");
         } else {
             System.out.println("Xung đột suất chiếu hoặc suất chiếu nằm ngoài khoảng ngày chiếu của phim. Không thể lưu.");
-//            return schedule.getId();
+            return schedule.getId();
         }
 //        suất chiếu phải nằm trong khoảng ngày bắt đầu và kết thúc của phim
 
 //            System.out.println(finishAt);
-        return null;
+        return id;
 
     }
 
-    //        @Scheduled(cron = "* * * * * *")
+//    @Async
+//    @Scheduled(fixedRate = 60000)
     public void scheduleFixedRate() {
         // danh sách lịch chiếu
         List<Schedule> listSchedule = repository.findAll();
@@ -118,6 +139,7 @@ public class ScheduleServiceImpl implements ScheduleService {
         scheduleNew.setMovie(schedule.getMovie());
         scheduleNew.setName(schedule.getName());
         scheduleNew.setStartAt(schedule.getStartAt());
+        scheduleNew.setPrice(checkTheDayOfTheWeek(schedule));
 //        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
         //lấy thời gian bắt đầu
         LocalDateTime startAt = schedule.getStartAt();
@@ -186,28 +208,74 @@ public class ScheduleServiceImpl implements ScheduleService {
 
     }
 
-    //     Lịch chiếu nằm trong khoảng bắt đầu và kết thúc của phim
-    public boolean timeSchedule(Schedule schedule) {
+    //     Lịch chiếu nằm trong khoảng thời gian mở rạp
+    public boolean timeSchedule(Schedule schedule, LocalTime startDay, LocalTime endDay) {
         LocalDateTime startAt = schedule.getStartAt();
         LocalDateTime finishAt = schedule.getFinishAt();
         LocalTime scheduleStart = LocalTime.from(startAt.atZone(ZoneId.systemDefault()));
         LocalTime scheduleFinish = LocalTime.from(finishAt.atZone(ZoneId.systemDefault()));
-        LocalTime startDay = LocalTime.of(8, 00);
-        LocalTime endDay = LocalTime.of(2, 00);
+//        LocalTime startDay = LocalTime.of(8, 00);
+//        LocalTime endDay = LocalTime.of(2, 00);
         if (scheduleStart.isBefore(endDay) && scheduleFinish.isBefore(endDay)) {
             System.out.println("1");
             return true;
-        } else if (scheduleStart.isAfter(startDay)||scheduleStart.equals(startDay) && scheduleFinish.isAfter(startDay)) {
+        } else if (scheduleStart.isAfter(startDay) || scheduleStart.equals(startDay) && scheduleFinish.isAfter(startDay)) {
             System.out.println("2");
             return true;
         } else if (scheduleStart.isBefore(endDay) && scheduleFinish.isAfter(startDay)) {
             System.out.println("3");
             return true;
         }
+        System.out.println("tui o day");
         return false;
     }
 
-    public List<Schedule> generateSchedule() throws ParseException {
+    //Kiểm tra và tăng giá suất chiếu
+    public BigDecimal checkTheDayOfTheWeek(Schedule schedule) {
+        BigDecimal fixedPrice = findByIdSetting().getFixedTicketPrice();
+        System.out.println(fixedPrice);
+        // Lấy mốc thời gian bắt đầu tăng giá trong ngày
+        LocalTime timeBeginsToChange = findByIdSetting().getTimeBeginsToChange();
+        // Mốc thời gian kết thúc tăng giá
+        LocalTime endDay = findByIdSetting().getCloseTime();
+        // Lấy tỷ lệ phần trăm tăng giá sau 17 giờ
+        double percentDay = (double) findByIdSetting().getPercentDay() / 100;
+        BigDecimal priceDay = fixedPrice.multiply(BigDecimal.valueOf(percentDay));
+        // Lấy tỷ lệ phần trăm tăng giá cuối tuần
+        double percentWeekend = (double) findByIdSetting().getPercentWeekend() / 100;
+        BigDecimal priceWeekend = fixedPrice.multiply(BigDecimal.valueOf(percentWeekend));
+        BigDecimal newPrice = null;
+        // Cho ngày dạng LocalDateTime
+//        LocalDateTime currentDate = LocalDateTime.of(2023, 8, 20, 15, 0); // ví dụ 20/08/2023 22:00
+        LocalDateTime currentDate = schedule.getStartAt();
+        // Kiểm tra xem ngày hiện tại có phải là cuối tuần hay không
+        if (currentDate.getDayOfWeek() == DayOfWeek.SATURDAY || currentDate.getDayOfWeek() == DayOfWeek.SUNDAY) {
+            // Kiểm tra xem giờ bắt đầu của lịch có lớn hơn hoặc bằng thời gian bắt đầu thay đổi không
+            if (currentDate.getHour() >= timeBeginsToChange.getHour() && currentDate.getHour() <= endDay.getHour()) {
+                // Thay đổi giá theo tỷ lệ phần trăm tăng giá cuối tuần sau 17 giờ
+                newPrice = fixedPrice.add(priceWeekend).add(priceDay);
+                System.out.println("Giá vé cuối tuần sau 17:00: " + newPrice);
+            } else {
+                // Thay đổi giá theo tỷ lệ phần trăm tăng giá cuối tuần
+                newPrice = fixedPrice.add(priceWeekend);
+                System.out.println("Giá vé cuối tuần: " + newPrice);
+            }
+        } else {
+            // Kiểm tra xem giờ bắt đầu của lịch có lớn hơn hoặc bằng thời gian bắt đầu thay đổi và nhỏ hơn hoặc bằng 2 không
+            if (currentDate.getHour() >= timeBeginsToChange.getHour() && currentDate.getHour() <= endDay.getHour()) {
+                // Thay đổi giá theo tỷ lệ phần trăm tăng giá sau 17 giờ
+                newPrice = fixedPrice.add(priceDay);
+                System.out.println("Giá vé sau 17:00: " + newPrice);
+            } else {
+                // In giá cố định
+                newPrice = fixedPrice;
+                System.out.println("Giá vé cố định: " + fixedPrice);
+            }
+        }
+        return newPrice;
+    }
+
+    public List<Schedule> generateSchedule() {
         List<Schedule> newList = new ArrayList<>();
         LocalDateTime startTime = LocalDateTime.of(2023, 8, 20, 8, 0); // Thời gian bắt đầu đầu tiên
         LocalDateTime endTime = LocalDateTime.of(2023, 8, 21, 2, 0); // Thời gian kết thúc
