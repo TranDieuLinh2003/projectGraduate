fff7bae35b5a5bad8285af6313f9a630ee43cab8
DuongTanjiroKamado <141843752+DuongDoremon@users.noreply.github.com>
2023-11-16 16:43:40 +0700

    Update HomeController.java
    update homeController

--- src/main/java/com/example/filmBooking/controller/HomeController.java
+++ src/main/java/com/example/filmBooking/controller/HomeController.java
@@ -1,14 +1,26 @@
 package com.example.filmBooking.controller;
 
+import com.example.filmBooking.model.Cinema;
+import com.example.filmBooking.service.CinemaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+
+    @Autowired
+    private CinemaService cinemaService;
+
     @GetMapping
-    public String home(){
+    public String home(Model model) {
+        List<Cinema> listCinema = cinemaService.fillAll();
+        model.addAttribute("listCinema", listCinema);
         return "admin/home";
     }
 }
