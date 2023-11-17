668488bc2fb11ff9f7079d56e75a90925a45df63
DuongTanjiroKamado <141843752+DuongDoremon@users.noreply.github.com>
2023-11-17 21:02:40 +0700

    Update BillRepository.java
    update billRepo

--- src/main/java/com/example/filmBooking/repository/BillRepository.java
+++ src/main/java/com/example/filmBooking/repository/BillRepository.java
@@ -21,4 +21,31 @@ public interface BillRepository extends JpaRepository<Bill, String> {
 
     @Query("SELECT COUNT(b) FROM Bill b WHERE b.status = 0")
     String countSoldTicketsWithStatusZero();
+    
+    @Query(value = "select sum(total_money) from bill b join" +
+            "(SELECT DISTINCT bill_id, c.id, c.name FROM bill_ticket bt " +
+            "JOIN ticket t ON t.id = bt.ticket_id" +
+            " JOIN schedule s ON t.schedule_id = s.id" +
+            " JOIN room r ON s.room_id = r.id" +
+            " JOIN cinema c ON c.id = r.cinema_id) d on d.bill_id = b.id" +
+            " WHERE " +
+            " d.id=?1 and"+
+            "    b.date_create >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) "+
+            "GROUP BY DATE(b.date_create), d.id = ?1"+
+            " ORDER BY DATE(b.date_create) DESC "
+            , nativeQuery = true)
+    List<BigDecimal> revenueInTheLast7Days(String cinemaId);
+
+    @Query(value = " SELECT m.name, COUNT(bt.id) AS bill_ticket_count " +
+            " FROM bill_ticket bt" +
+            " JOIN  bill b ON bt.bill_id = b.id" +
+            " JOIN ticket t ON bt.ticket_id = t.id" +
+            " JOIN schedule s ON t.schedule_id = s.id" +
+            " JOIN movie m ON s.movie_id = m.id" +
+            " WHERE MONTH(b.date_create) = MONTH(CURDATE()) " +
+            "    AND YEAR(b.date_create) = YEAR(CURDATE())" +
+            " GROUP BY m" +
+            " ORDER BY bill_ticket_count DESC" +
+            " LIMIT 5;", nativeQuery = true)
+    List<Object[]> listTop5Movie();
 }
