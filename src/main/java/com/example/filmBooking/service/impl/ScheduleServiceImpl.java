fff280cca55c76acbf6a2536be71037272c41cb4
TranThiDieuLinh <110156879+TranThiDieuLinh@users.noreply.github.com>
2023-12-08 14:35:10 +0700

    update search schedule admin

--- src/main/java/com/example/filmBooking/service/impl/ScheduleServiceImpl.java
+++ src/main/java/com/example/filmBooking/service/impl/ScheduleServiceImpl.java
@@ -408,8 +408,8 @@ public class ScheduleServiceImpl implements ScheduleService {
     }
 
     @Override
-    public Page<Schedule> searchSchedule(String name, LocalDate startAt, String movieName, Integer startTime, Integer endTime, Integer currentPage) {
-        return repository.searchBySchedule(name, startAt, movieName, startTime, endTime, pageSchedule(currentPage));
+    public Page<Schedule> searchSchedule(String name, LocalDate startAt, String movieName, Integer startTime, Integer endTime,String status, Integer currentPage) {
+        return repository.searchBySchedule(name, startAt, movieName, startTime, endTime, status, pageSchedule(currentPage));
     }
 
     public void autoSave(String idSchedule) {
