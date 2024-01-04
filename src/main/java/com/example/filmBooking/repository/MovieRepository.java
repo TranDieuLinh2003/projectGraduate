12f35ecacc03a9610b2e06dd5ade7899fb3dd24a
ducnguyen0213 <quaduachin132@gmail.com>
2024-01-04 23:17:40 +0700

    update movie

--- src/main/java/com/example/filmBooking/repository/MovieRepository.java
+++ src/main/java/com/example/filmBooking/repository/MovieRepository.java
@@ -13,16 +13,19 @@ import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, String> {
 
-    String showPhimDangChieu = "select * from projectLinh.movie where status like 'Đang chiếu'; " ;
+    String showPhimDangChieu = "select * from projectLinh.movie where status like 'Đang chiếu'; ";
+
     @Query(value = showPhimDangChieu, nativeQuery = true)
     List<Movie> showPhimDangChieu();
 
 
-    String showPhimSapChieu = "select * from projectLinh.movie where status like 'Sắp chiếu'; " ;
+    String showPhimSapChieu = "select * from projectLinh.movie where status like 'Sắp chiếu'; ";
+
     @Query(value = showPhimSapChieu, nativeQuery = true)
     List<Movie> showPhimSapChieu();
 
-    String showPhimSapChieuAndDangChieu = "select * from projectLinh.movie where status IN ('Sắp chiếu', 'Đang chiếu');" ;
+    String showPhimSapChieuAndDangChieu = "select * from projectLinh.movie where status IN ('Sắp chiếu', 'Đang chiếu');";
+
     @Query(value = showPhimSapChieuAndDangChieu, nativeQuery = true)
     List<Movie> showPhimSapChieuAndDangChieu();
 
@@ -33,16 +36,17 @@ public interface MovieRepository extends JpaRepository<Movie, String> {
             "\tjoin projectLinh.movie m on m.id = s.movie_id\n" +
             "where m.id=:movieId\n" +
             " and c.id =:cinemaId");
+
     @Query(value = movie, nativeQuery = true)
     List<Movie> getMovie(@Param("movieId") String movieId, @Param("cinemaId") String cinemaId);
-    
+
     Page<Movie> findByNameContains(String keyword, Pageable pageable);
 
     Movie findByNameLike(String name);
 
     @Query(value = "SELECT m FROM Movie m WHERE (:status IS NULL OR m.status IS NULL OR m.status like %:status%) " +
             "AND (:name IS NULL OR m.name IS NULL OR m.name LIKE %:name%)")
-    Page<Movie> findAllByStatusAndName(@Param("status") String status,@Param("name") String keyword, Pageable pageable);
+    Page<Movie> findAllByStatusAndName(@Param("status") String status, @Param("name") String keyword, Pageable pageable);
 
     @Query(value = "SELECT DISTINCT m FROM Movie m " +
             "LEFT JOIN m.directors d " +
@@ -56,4 +60,22 @@ public interface MovieRepository extends JpaRepository<Movie, String> {
             "OR type.name LIKE %:keyword% " +
             "OR performer.name LIKE %:keyword%)")
     Page<Movie> findAllByStatusAndNameAndKeyWord(@Param("status") String status, @Param("keyword") String keyword, Pageable pageable);
+
+    @Query(value = "SELECT DISTINCT m\n" +
+            "FROM Movie m\n" +
+            "LEFT JOIN m.directors d " +
+            "LEFT JOIN m.languages lang " +
+            "LEFT JOIN m.movieTypes type " +
+            "LEFT JOIN m.performers performer " +
+            "WHERE (?1 IS NULL OR d.id IN ?1) \n" +
+            "AND (?2 IS NULL OR lang.id IN ?2) \n" +
+            "AND (?3 IS NULL OR type.id IN ?3) \n" +
+            "AND (?4 IS NULL OR performer.id IN ?4)")
+    Page<Movie> filterMovies(Pageable pageable,
+                             @Param("directors") String directors,
+                             @Param("languages") String languages,
+                             @Param("movieTypes") String movieTypes,
+                             @Param("performers") String performers);
+
+
 }
